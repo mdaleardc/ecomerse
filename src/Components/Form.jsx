fbbaaeb7aee12f01df21c1b936c15a60656d1f2f
mdaleardc <ismailaliamot@gmail.com>
2024-11-10 20:28:13 +0600

    Updated Form.jsx
    Updated form validation

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,54 +1,80 @@
-import React, { useState } from "react"
+import React, {
+  useState
+} from "react"
 
 const Form = () => {
+
+  const [userData,
+    setUserData] = useState({
+      userName: "",
+      email: "",
+      number: "",
+      createPass: "",
+      confirmPass: "",
+
+    });
   
-  const [firstName, setFirstName] = useState("Dev");
-  const [lastName, setLastName] = useState("Code");
-  const [number, setNumber] = useState(0);
-  const [newPass, setNewPass] = useState("password");
-  const [passAgain, setPassAgain] = useState("password");
-  
+  const [warn, setWarn] = useState({});
+  const [flag, setFlag] = useState(false);
+
   const handleChange = (e) => {
     e.preventDefault();
-    const targetName = e.target.name;
-    const targetVal = e.target.value;
-    if(targetName === "f-name") {
-      setFirstName(targetVal)
-    } else if(targetName === "l-name") {
-      setLastName(targetVal);
-    } else if(targetName === "number") {
-      setNumber(targetVal);
-    } else if(targetName === "new-pass") {
-      setNewPass(targetVal);
-    } else if(targetName === "match-pass") {
-      setPassAgain(targetVal);
+    
+    setUserData({...userData, [e.target.name] : e.target.value});
+
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const warnObj = {};
+    
+    if (userData.userName === "") {
+      warnObj.userName = "Please enter the username!";
     }
+    if (userData.email === "") {
+      warnObj.email = "Please enter your email!";
+    }
+    if (userData.number === "") {
+      warnObj.number = "Please enter phone number!";
+    }
+    if (userData.createPass === "") {
+      warnObj.createPass = "Please enter a password!";
+    }
+    if (userData.confirmPass === "") {
+      warnObj.confirmPass = "Please enter confirm password!";
+    } else {
+      setFlag(true);
+    }
+    
+    setWarn(warnObj);
   }
   
   return (
     <div>
-    <form action="" className="form-section p-[1rem]  bg-[#123] rounded m-y-[1rem]">
-      
-      <fieldset className="border-[3px] border-solid border-[#2ef]">
-        <legend className="text-3xl font-[bold] text-[#4ef] mx-[auto] my-[0]">Sign Up</legend>
-          
-        <div className="grid grid-rows gap-x-[2rem] gap-y-[0.5rem] md:grid-cols-2 md:w-[100%] justify-center content-center">
-      <label for="f-name">First Name: </label>
-      <input type="text" id="f-name" name="f-name" value={firstName} onChange={handleChange}/>
-      <label for="l-name">Last Name: </label>
-      <input type="text" id="l-name" name="l-name" value={lastName} onChange={handleChange}/>
-      <label for="number">Number: </label>
-      <input type="number" id="number" name="number" value={number} onChange={handleChange}/>
-      <label for="new-pass">New Password: </label>
-      <input type="password" id="new-pass" name="new-pass" value={newPass} onChange={handleChange}/>
-      <label for="match-pass">Password Again: </label>
-      <input type="password" id="match-pass" name="match-pass" value={passAgain} onChange={handleChange}/>
-        </div>
-      </fieldset>
-      <input type="submit" className="w-[5rem] rounded my-[0] mx-[auto] text-center block mt-[2rem] bg-[#00A4FF]"/>
+    <form className="form-section p-[1rem]  bg-[#123e] rounded-[1rem] my-[1rem] w-[95%] md:w-[50%]" onSubmit={handleSubmit}>
+    <h1 className="text-1xl text-center text-[#FFFFFF]">{flag ? <p>{userData.userName} you have registered successfuly!</p> : ""}</h1>
+      <fieldset className="my-[0] mx-[auto] block">
+        <div className="text-3xl font-[bold] text-[#4ef] text-center">
+Sign Up
+    </div>
+        <div className="my-[0] mx-[auto] flex flex-col gap-[0.5rem]">
+      <input type="text" className="text-[#000] font-bold pl-[0.5rem] username" name="userName" placeholder="Username" autoComplete="off" value={userData.userName} onChange={handleChange} />
+      <span className="text-[red]">{warn.userName}</span>
+      <input type="text" className="text-[#000] font-bold pl-[0.5rem] email" name="email" placeholder="Email" autoComplete="off" value={userData.email} onChange={handleChange} />
+      <span className="text-[red]">{warn.email}</span>
+      <input type="text" className="text-[#000] font-bold pl-[0.5rem] number" name="number" placeholder="Phone number" autoComplete="off" value={userData.number} onChange={handleChange} />
+      <span className="text-[red]">{warn.number}</span>
+      <input type="password" className="text-[#000] font-bold pl-[0.5rem] creat-pass" name="createPass" placeholder="Create password" autoComplete="off" value={userData.createPass} onChange={handleChange} />
+      <span className="text-[red]">{warn.createPass}</span>
+      <input type="password" className="text-[#000] font-bold pl-[0.5rem] confirm-pass" name="confirmPass" placeholder="Confirm password" autoComplete="off" value={userData.confirmPass} onChange={handleChange} />
+      <span className="text-[red]">{warn.confirmPass}</span>
+    </div>
+    </fieldset>
+      <button type="submit" className="w-[5rem] rounded my-[0] mx-[auto] text-center block mt-[1rem] bg-[#00A4FF]">Sign Up</button>
     </form>
     </div>
-    )
+  )
 }
 
 export default Form;
