56407d36d0273044fb23598db65f20aa6b19b5f0
mdaleardc <ismailaliamot@gmail.com>
2024-11-11 19:28:53 +0600

    Updated Form.jsx
    Updated password and email validation

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -11,7 +11,6 @@ const Form = () => {
       number: "",
       createPass: "",
       confirmPass: "",
-
     });
   
   const [warn, setWarn] = useState({});
@@ -29,21 +28,38 @@ const Form = () => {
     
     const warnObj = {};
     
+    const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+    const isValidEmail = emailRegex.test(userData.email);
+    
     if (userData.userName === "") {
       warnObj.userName = "Please enter the username!";
+    } else if (userData.userName.length <= 3 || userData.userName.length >= 30) {
+      warnObj.userName = "Username must be between 4 to 30 characters!";
     }
     if (userData.email === "") {
       warnObj.email = "Please enter your email!";
+    } else if (!isValidEmail) {
+      warnObj.email = "Please enter valid email address!";
     }
     if (userData.number === "") {
       warnObj.number = "Please enter phone number!";
+    } else if (!/^\d{11}$/.test(userData.number)) {
+      warnObj.number = "Number must be only digits up to 11!"
     }
     if (userData.createPass === "") {
       warnObj.createPass = "Please enter a password!";
+    } else if (userData.createPass.length <= 5 || userData.createPass.length >= 17) {
+      warnObj.createPass = "Password must be between 6 to 16 characters long!";
     }
     if (userData.confirmPass === "") {
       warnObj.confirmPass = "Please enter confirm password!";
-    } else {
+    } else if (userData.confirmPass !== userData.createPass) {
+      warnObj.confirmPass = "Password dit not match!"
+    }
+      else {
       setFlag(true);
     }
     
@@ -52,26 +68,26 @@ const Form = () => {
   
   return (
     <div>
-    <form className="form-section p-[1rem]  bg-[#123e] rounded-[1rem] my-[1rem] w-[95%] md:w-[50%]" onSubmit={handleSubmit}>
-    <h1 className="text-1xl text-center text-[#FFFFFF]">{flag ? <p>{userData.userName} you have registered successfuly!</p> : ""}</h1>
+    <form className="form-section p-[1rem]  bg-[#fff1] border-[2px] rounded my-[1rem] w-[95%] md:w-[50%]" onSubmit={handleSubmit}>
+    <h1 className="text-1xl text-center text-[#345]">{flag ? <p>{userData.userName} you have registered successfuly!</p> : ""}</h1>
       <fieldset className="my-[0] mx-[auto] block">
-        <div className="text-3xl font-[bold] text-[#4ef] text-center">
+        <div className="text-3xl font-bold text-[#4ef] text-center my-[1rem]">
 Sign Up
     </div>
-        <div className="my-[0] mx-[auto] flex flex-col gap-[0.5rem]">
-      <input type="text" className="text-[#000] font-bold pl-[0.5rem] username" name="userName" placeholder="Username" autoComplete="off" value={userData.userName} onChange={handleChange} />
+        <div className="my-[0] mx-[auto] flex flex-col gap-[0.5rem] justify-center items-center">
+      <input type="text" className="text-[#000] border-[2px] rounded font-bold pl-[0.5rem] username" name="userName" placeholder="Username" autoComplete="off" value={userData.userName} onChange={handleChange} />
       <span className="text-[red]">{warn.userName}</span>
-      <input type="text" className="text-[#000] font-bold pl-[0.5rem] email" name="email" placeholder="Email" autoComplete="off" value={userData.email} onChange={handleChange} />
+      <input type="text" className="text-[#000] border-[2px] rounded font-bold pl-[0.5rem] email" name="email" placeholder="Email" autoComplete="off" value={userData.email} onChange={handleChange} />
       <span className="text-[red]">{warn.email}</span>
-      <input type="text" className="text-[#000] font-bold pl-[0.5rem] number" name="number" placeholder="Phone number" autoComplete="off" value={userData.number} onChange={handleChange} />
+      <input type="text" className="text-[#000] border-[2px] rounded font-bold pl-[0.5rem] number" name="number" placeholder="Phone number" autoComplete="off" value={userData.number} onChange={handleChange} />
       <span className="text-[red]">{warn.number}</span>
-      <input type="password" className="text-[#000] font-bold pl-[0.5rem] creat-pass" name="createPass" placeholder="Create password" autoComplete="off" value={userData.createPass} onChange={handleChange} />
+      <input type="text" className="text-[#000] border-[2px] rounded font-bold pl-[0.5rem] creat-pass" name="createPass" placeholder="Create password" autoComplete="off" value={userData.createPass} onChange={handleChange} />
       <span className="text-[red]">{warn.createPass}</span>
-      <input type="password" className="text-[#000] font-bold pl-[0.5rem] confirm-pass" name="confirmPass" placeholder="Confirm password" autoComplete="off" value={userData.confirmPass} onChange={handleChange} />
+      <input type="text" className="text-[#000] border-[2px] rounded font-bold pl-[0.5rem] confirm-pass" name="confirmPass" placeholder="Confirm password" autoComplete="off" value={userData.confirmPass} onChange={handleChange} />
       <span className="text-[red]">{warn.confirmPass}</span>
     </div>
     </fieldset>
-      <button type="submit" className="w-[5rem] rounded my-[0] mx-[auto] text-center block mt-[1rem] bg-[#00A4FF]">Sign Up</button>
+      <button type="submit" className="w-[5rem] rounded my-[0] mx-[auto] py-[0.4rem] text-white font-bold text-center block mt-[1rem] bg-[#00A4FF]">Sign Up</button>
     </form>
     </div>
   )
